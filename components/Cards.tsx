b4b6bd55c306d88e3e24d1d42c753d7278f7b740
shebl <shebblloll@gmail.com>
2023-08-22 12:39:27 +0300

    fix people card :after  when hover in mobile v.

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -7,7 +7,7 @@ function Cards() {
       <h1 className="font-bold text-3xl lg:text-5xl uppercase text-center mb-10  ">Join thousands of inspiring creatives</h1>
       <h1 className="text-gray-500 text-lg  text-center mb-14 lg:text-2xl ">You Can Create Awesome Like These</h1>
 
-      <div className="flex gap-2 lg:gap-6 justify-center items-center flex-col md:flex-row  ">
+      <div className="flex gap-16 lg:gap-6 justify-center items-center flex-col md:flex-row  ">
           <div className="flex gap-2 lg:gap-6 justify-center items-center flex-row  ">
             <div className="card ">
               <Image height={80} width={80} src="/x.jpg" alt="image"
@@ -36,7 +36,7 @@ function Cards() {
               className="cursor-pointer rounded-full " />
             </div>
             <div className="card">
-              <Image height={80} width={80} src="/6.png" alt="image"
+              <Image height={80} width={80} src="/1.png" alt="image"
               className="cursor-pointer rounded-full " />
             </div>
             <div className="card">
